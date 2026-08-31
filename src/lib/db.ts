f0fdfe242a33dc2774022d53e36f289f28a745e2
Papa Badu <raybadu10@gmail.com>
2026-08-31 11:12:06 +0000

    feat: add Dexie local database schema

---
+++ src/lib/db.ts
@@ -0,0 +1,29 @@
+import Dexie, { type Table } from 'dexie';
+import type { Animal, Plot, FarmEvent } from './types';
+
+export interface LocalAnimal extends Animal {
+  synced: 0 | 1;
+}
+export interface LocalPlot extends Plot {
+  synced: 0 | 1;
+}
+export interface LocalEvent extends FarmEvent {
+  synced: 0 | 1;
+}
+
+export class FarmDB extends Dexie {
+  animals!: Table<LocalAnimal, string>;
+  plots!: Table<LocalPlot, string>;
+  events!: Table<LocalEvent, string>;
+
+  constructor() {
+    super('farm-db');
+    this.version(1).stores({
+      animals: 'id, type, status, synced, updated_at',
+      plots: 'id, synced, updated_at',
+      events: 'id, client_id, entity_type, entity_id, event_type, event_date, synced, updated_at',
+    });
+  }
+}
+
+export const db = new FarmDB();
